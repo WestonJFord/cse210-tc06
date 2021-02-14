c31a22d637c870a4a9b04a4249085672acbae457
WestonJFord <westonford@outlook.com>
2021-02-13 19:08:50 -0700

    Got basic imports running smoothly

--- mastermind/game/director.py
+++ mastermind/game/director.py
@@ -2,3 +2,25 @@
 #CSE 210 Adam Hayes
 #Unit 6
 
+class Director:
+    def __init__(self):
+        from .board import Board
+        self.board = Board()
+        from .console import Console
+        self.console = Console()
+        from .move import Move
+        self.move = Move()
+        from .player import Player
+        self.player = Player()
+        from .roster import Roster
+        self.roster = Roster()
+
+    def test_import(self):
+        self.board.test_import()
+        self.console.test_import()
+        self.move.test_import()
+        self.player.test_import()
+        self.roster.test_import()
+
+    def start_game(self):
+        pass
--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -1,6 +1,11 @@
 class Player():
     '''set the move for the player'''
-
+    
+    def __init__(self):
+        pass
+    def test_import(self):
+        print('Player')
+        
     def set_code(self):
         self.player = Roster.get_current()
         self.code = Move.get_code()
